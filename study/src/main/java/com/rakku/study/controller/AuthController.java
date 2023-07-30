9efcf35e83c09a2aef21cb81d18ba429bc548db4
Rakku <dudfkr7845@gmail.com>
2023-07-30 16:09:16 +0900

    auth controller 및 html 구현

--- study/src/main/java/com/rakku/study/controller/AuthController.java
+++ study/src/main/java/com/rakku/study/controller/AuthController.java
@@ -10,4 +10,9 @@ public class AuthController {
     public String login(){
         return "auth/login";
     }
+
+    @GetMapping("/sign-up")
+    public String signUp(){
+        return "auth/sign-up";
+    }
 }
